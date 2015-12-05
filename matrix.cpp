240dab9e04ada4e194a401ebde5810e1285a669a
Dimitri Walters <me@dimi.io>
2015-12-05 13:43:06 -0500

    Add rotate matrix

--- matrix.cpp
+++ matrix.cpp
@@ -5,7 +5,16 @@ using namespace std;
 void printMatrix(int *matrix, int rows, int columns) {
     for (int i=0; i<rows; i++) {
         for (int j=0; j<columns; j++) {
-            cout << matrix[i*columns + j] << endl;
+            cout << matrix[i*columns + j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void rotate90(int *matrix, int rows, int columns, int *dest) {
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<columns; j++) {
+            dest[j*rows + i] = matrix[((rows - 1) - i) * columns + j];
         }
     }
 }
@@ -16,6 +25,9 @@ int main() {
         {4,5,6},
         {7,8,9}
     };
+    int newMatrix[3][3];
 
     printMatrix((int *)matrix, 3, 3);
+    rotate90((int *)matrix, 3, 3, (int *)newMatrix);
+    printMatrix((int *)newMatrix, 3, 3);
 }
