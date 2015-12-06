2eb7bf035c2958c56d01d65a915f09681eb2b587
Dimitri Walters <me@dimi.io>
2015-12-06 13:23:35 -0500

    Add bfs

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,6 +12,7 @@ public:
     Graph(int v);
     void addEdge(int u, int v);
     vector<bool> DFS(int s);
+    vector<bool> BFS(int s);
 };
 
 Graph::Graph(int v) {
@@ -44,6 +46,39 @@ vector<bool> Graph::DFS(int s) {
     return DFSutil(s, visited);
 }
 
+vector<bool> Graph::BFS(int s) {
+    vector<bool> visited(V);
+    for (int i=0; i<V; i++) {
+        visited[i] = false;
+    }
+
+    queue<int> next;
+    next.push(s);
+
+    int current, maybe;
+    while (!next.empty()) {
+        current = next.front();
+        next.pop();
+        visited[current] = true;
+
+        for (int i=0; i<adj[current].size(); i++) {
+            maybe = adj[current][i];
+            if (!visited[maybe]) {
+                next.push(maybe);
+            }
+        }
+    }
+
+    return visited;
+}
+
+void printVectorBool(vector<bool> v) {
+    for (int i=0; i<v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     Graph g(4);
     g.addEdge(0, 1);
@@ -53,11 +88,11 @@ int main() {
     g.addEdge(2, 3);
     g.addEdge(3, 3);
 
-    vector<bool> results = g.DFS(2);
-    for (int i=0; i<results.size(); i++) {
-        cout << results[i] << " ";
-    }
-    cout << endl;
+    vector<bool> results = g.DFS(3);
+    printVectorBool(results);
+
+    vector<bool> results2 = g.BFS(3);
+    printVectorBool(results2);
 
     return 0;
 }
