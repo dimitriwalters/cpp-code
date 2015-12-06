452dd9faf877e4da5aeb0bd79f9cc65826cbc951
Dimitri Walters <me@dimi.io>
2015-12-06 15:25:59 -0500

    Add topological sort

--- graph.cpp
+++ graph.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
+// HELPER FUNCTIONS
 void printArrayBool(bool arr[], int length) {
     for (int i=0; i<length; i++) {
         cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void printStack(stack<int> s) {
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
     }
     cout << endl;
 }
@@ -15,11 +24,13 @@ class Graph {
     int V;
     vector<int> *adj;
     void DFSutil(int s, bool visited[]);
+    void topSortUtil(int s, bool visited[], stack<int> &finish);
 public:
     Graph(int v);
     void addEdge(int u, int v);
     void DFS(int s);
     void BFS(int s);
+    void topSort();
 };
 
 Graph::Graph(int v) {
@@ -78,17 +89,46 @@ void Graph::BFS(int s) {
     printArrayBool(visited, V);
 }
 
+void Graph::topSortUtil(int s, bool visited[], stack<int> &finish) {
+    visited[s] = true;
+
+    for (int i=0; i<adj[s].size(); i++) {
+        if (!visited[adj[s][i]]) {
+            topSortUtil(adj[s][i], visited, finish);
+        }
+    }
+
+    finish.push(s);
+}
+
+void Graph::topSort() {
+    bool visited[V];
+    for (int i=0; i<V; i++) {
+        visited[i] = false;
+    }
+
+    stack<int> finish;
+
+    for (int i=0; i<V; i++) {
+        if (!visited[i]) {
+            topSortUtil(i, visited, finish);
+        }
+    }
+
+    printStack(finish);
+}
+
 int main() {
-    Graph g(4);
+    Graph g(6);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 2);
-    g.addEdge(2, 0);
     g.addEdge(2, 3);
-    g.addEdge(3, 3);
+    g.addEdge(5, 4);
 
     g.DFS(3);
     g.BFS(3);
+    g.topSort();
 
     return 0;
 }
