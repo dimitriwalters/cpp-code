034f83395742135703c1cfab3b316c0ec22483a9
Dimitri Walters <me@dimi.io>
2015-12-07 13:16:01 -0500

    Add search matrix

--- matrix.cpp
+++ matrix.cpp
@@ -92,6 +92,30 @@ bool isWordInMatrix(char *matrix, int rows, int columns, string word) {
     return false;
 }
 
+bool isTargetInMatrix(int *matrix, int rows, int columns, int target) {
+    int start = 0;
+    int end = rows*columns - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        int midX = mid / columns;
+        int midY = mid % columns;
+
+        // // DEBUG
+        // cout << "mid " << mid << " " << midX << " " << midY << endl;
+
+        if (matrix[midX*columns + midY] == target) {
+            return true;
+        } else if (matrix[midX*columns + midY] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return false;
+}
+
 int main() {
     int matrix[3][3] = {
         {1,2,3},
@@ -112,4 +136,19 @@ int main() {
     printCharMatrix((char *)a, 3, 3);
 
     cout << isWordInMatrix((char *)a, 3, 3, "aba") << endl;
+
+    int bigMatrix[10][10] = {
+        {1,2,3,4,5,6,7,8,9,10},
+        {11,12,13,14,15,16,17,18,19,20},
+        {21,22,23,24,25,26,27,28,29,30},
+        {31,32,33,34,35,36,37,38,39,40},
+        {41,42,43,44,45,46,47,48,49,50},
+        {51,52,53,54,55,56,57,58,59,60},
+        {61,62,63,64,65,66,67,68,69,70},
+        {71,72,73,74,75,76,77,78,79,80},
+        {81,82,83,84,85,86,87,88,89,90},
+        {91,92,93,94,95,96,97,98,99,100}
+    };
+
+    cout << isTargetInMatrix((int *)bigMatrix, 10, 10, 100) << endl;
 }
