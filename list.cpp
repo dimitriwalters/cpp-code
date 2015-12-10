13900f282735d2feb8a5d183f7c0e62297e49867
Dimitri Walters <me@dimi.io>
2015-12-10 17:27:01 -0500

    Fix list for delete item method

--- list.cpp
+++ list.cpp
@@ -16,6 +16,7 @@ public:
     ~List();
     void push_front(int v);
     void push_back(int v);
+    void delete_item(int v);
     void print();
 };
 
@@ -40,7 +41,11 @@ void List::push_front(int v) {
     newnode->next = head;
     newnode->prev = nullptr;
 
+    if (head != nullptr) {
+        head->prev = newnode;
+    }
     head = newnode;
+
     if (tail == nullptr) {
         tail = head;
     }
@@ -62,6 +67,36 @@ void List::push_back(int v) {
     }
 }
 
+void List::delete_item(int v) {
+    node *current, *next, *prev;
+    current = head;
+    while (current != nullptr) {
+        prev = current->prev;
+        next = current->next;
+        if (current->item == v) {
+            if (prev == nullptr && next == nullptr) {
+                head = nullptr;
+                tail = head;
+            } else if (prev == nullptr) {
+                head = next;
+                next->prev = nullptr;
+            } else if (next == nullptr) {
+                tail = prev;
+                prev->next = nullptr;
+            } else {
+                prev->next = next;
+                next->prev = prev;
+            }
+
+            delete current;
+            return;
+        } else {
+            prev = current;
+            current = next;
+        }
+    }
+}
+
 void List::print() {
     node *current, *next;
     current = head;
@@ -76,7 +111,16 @@ void List::print() {
 int main() {
     List list;
     list.push_front(5);
+    list.push_front(9);
     list.push_front(8);
     list.push_back(11);
+    list.print();
+    list.delete_item(9);
+    list.print();
+    list.delete_item(8);
+    list.print();
+    list.delete_item(11);
+    list.print();
+    list.delete_item(5);
     list.print();
 }
