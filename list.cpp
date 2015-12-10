03fd6f637a18b5f5aa4eebb31ea20ae627b651f4
Dimitri Walters <me@dimi.io>
2015-12-10 16:20:06 -0500

    Implement doubly linked list

--- list.cpp
+++ list.cpp
@@ -5,14 +5,17 @@ using namespace std;
 struct node {
     int item;
     node *next;
+    node *prev;
 };
 
 class List {
     node *head;
+    node *tail;
 public:
     List(int v);
     ~List();
     void push_front(int v);
+    void push_back(int v);
     void print();
 };
 
@@ -20,6 +23,8 @@ List::List(int v) {
     head = new node;
     head->item = v;
     head->next = nullptr;
+    head->prev = nullptr;
+    tail = head;
 }
 
 List::~List() {
@@ -36,8 +41,19 @@ void List::push_front(int v) {
     node *newnode = new node;
     newnode->item = v;
     newnode->next = head;
+    newnode->prev = nullptr;
 
     head = newnode;
+}
+
+void List::push_back(int v) {
+    node *newnode = new node;
+    newnode->item = v;
+    newnode->next = nullptr;
+    newnode->prev = tail;
+
+    tail->next = newnode;
+    tail = newnode;
 }
 
 void List::print() {
@@ -54,5 +70,6 @@ void List::print() {
 int main() {
     List list(5);
     list.push_front(8);
+    list.push_back(11);
     list.print();
 }
